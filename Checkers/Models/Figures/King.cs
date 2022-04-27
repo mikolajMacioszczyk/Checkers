c64780e2e2bc1810743fd561c24e32200dc48193
Mikołaj Macioszczyk <macioszczyk@vialutions.com>
2022-04-27 14:31:07 +0200

    retrive king moves

--- Checkers/Models/Figures/King.cs
+++ Checkers/Models/Figures/King.cs
@@ -10,7 +10,7 @@ namespace Checkers.Models
         {
             var moves = new List<MoveBase>();
 
-            //TryAddKillMoves(board, moves, new List<KillMove>(), CurrentPosition.Row, CurrentPosition.Column);
+            TryAddKillMoves(board, moves);
             if (!moves.Any())
             {
                 TryAddNormalMoves(board, moves);
@@ -18,6 +18,8 @@ namespace Checkers.Models
 
             return moves;
         }
+
+        #region Normal move
 
         private void TryAddNormalMoves(Board board, List<MoveBase> moves)
         {
@@ -50,5 +52,136 @@ namespace Checkers.Models
                 nextColumn = columnChange(nextColumn);
             }
         }
+
+        #endregion
+
+        #region Kill move
+
+        private void TryAddKillMoves(Board board, List<MoveBase> moves)
+        {
+            var firstKills = new List<(bool, KillMove)> 
+            {
+                // move left top
+                TryAddFirstKillMove(row => row + 1, column => column - 1, board),
+                // move right top
+                TryAddFirstKillMove(row => row + 1, column => column + 1, board),
+                // move left down
+                TryAddFirstKillMove(row => row - 1, column => column - 1, board),
+                // move right down
+                TryAddFirstKillMove(row => row - 1, column => column + 1, board)
+            };
+
+            foreach (var firstKill in firstKills)
+            {
+                if (firstKill.Item1)
+                {
+                    TryAddKillMovesWithStrike(board, moves, new List<KillMove> { firstKill.Item2 }, 
+                        firstKill.Item2.Target.Row, firstKill.Item2.Target.Column);
+                }
+            }
+        }
+
+        private (bool, KillMove) TryAddFirstKillMove(Func<int, int> rowChange, Func<int, int> columnChange, Board board)
+        {
+            var enemyRow = rowChange(CurrentPosition.Row);
+            var enemyColumn = columnChange(CurrentPosition.Column);
+            var nextRow = rowChange(enemyRow);
+            var nextColumn = columnChange(enemyColumn);
+
+            while (nextRow <= board.Size - 1
+                && nextRow >= 0
+                && nextColumn <= board.Size - 1
+                && nextColumn >= 0
+                && board.Positions[enemyRow, enemyColumn].IsEmpty())
+            {
+                enemyRow = rowChange(enemyRow);
+                enemyColumn = columnChange(enemyColumn);
+                nextRow = rowChange(nextRow);
+                nextColumn = columnChange(nextColumn);
+            }
+
+            if (nextRow >= board.Size
+                || nextRow < 0
+                || nextColumn >= board.Size
+                || nextColumn < 0
+                || board.Positions[enemyRow, enemyColumn].IsEmpty()
+                || board.Positions[enemyRow, enemyColumn].Figure.Color == Color
+                || !board.Positions[nextRow, nextColumn].IsEmpty())
+            {
+                return (false, null);
+            }
+
+            return (true, new KillMove(CurrentPosition, board.Positions[nextRow, nextColumn], board.Positions[enemyRow, enemyColumn]));
+        }
+
+        private void TryAddKillMovesWithStrike(Board board, List<MoveBase> moves, List<KillMove> strike, int currentRow, int currentColumn)
+        {
+            bool isStopped = true;
+            var currentPosition = board.Positions[currentRow, currentColumn];
+
+            // try leftTop
+            TryAddKillMoveWithStrike(currentRow + 1, currentColumn - 1, currentRow + 2, currentColumn - 2, ref isStopped,
+                currentPosition, (nextColumn, nextRow) => nextColumn >= 0 && nextRow < board.Size, board, moves, strike);
+
+            // try rightTop
+            TryAddKillMoveWithStrike(currentRow + 1, currentColumn + 1, currentRow + 2, currentColumn + 2, ref isStopped,
+                currentPosition, (nextColumn, nextRow) => nextColumn < board.Size && nextRow < board.Size, board, moves, strike);
+
+            // try leftDown
+            TryAddKillMoveWithStrike(currentRow - 1, currentColumn - 1, currentRow - 2, currentColumn - 2, ref isStopped,
+                currentPosition, (nextColumn, nextRow) => nextColumn >= 0 && nextRow >= 0, board, moves, strike);
+
+            // try rightDown
+            TryAddKillMoveWithStrike(currentRow - 1, currentColumn + 1, currentRow - 2, currentColumn + 2, ref isStopped,
+                currentPosition, (nextColumn, nextRow) => nextColumn < board.Size && nextRow >= 0, board, moves, strike);
+
+            // if movement is finished
+            StopKillMove(isStopped, moves, strike);
+        }
+
+        private void StopKillMove(bool isStopped, List<MoveBase> moves, List<KillMove> strike)
+        {
+            if (isStopped && strike.Any())
+            {
+                var resultMove = strike.First().Copy();
+                var current = resultMove;
+
+                foreach (var nextMove in strike.Skip(1))
+                {
+                    current.InnerMove = nextMove.Copy();
+                    current = current.InnerMove;
+                }
+                moves.Add(resultMove);
+            }
+        }
+
+        private void TryAddKillMoveWithStrike(int enemyRow, int enemyColumn, int nextRow, int nextColumn, ref bool isStopped,
+            Position currentPosition, Func<int, int, bool> predicate, Board board, List<MoveBase> moves, List<KillMove> strike)
+        {
+            if (predicate(nextColumn, nextRow))
+            {
+                var enemyPosition = board.Positions[enemyRow, enemyColumn];
+                if (enemyPosition != null
+                    // there should be figure
+                    && enemyPosition.Figure != null
+                    // not from my team
+                    && enemyPosition.Figure.Color != Color
+                    // check if not want to kill the same position second time
+                    && !strike.Any(s => s.Killed == enemyPosition))
+                {
+                    var nextPosition = board.Positions[nextRow, nextColumn];
+                    if (nextPosition != null && (nextPosition.Figure == null || nextPosition.Figure == this))
+                    {
+                        // move is continued
+                        isStopped = false;
+                        strike.Add(new KillMove(currentPosition, nextPosition, enemyPosition));
+                        TryAddKillMovesWithStrike(board, moves, strike, nextRow, nextColumn);
+                        strike.RemoveAt(strike.Count - 1);
+                    }
+                }
+            }
+        }
+
+        #endregion
     }
 }
--- CheckersTests/MovesTests/KingGetAvailableMovesTests.cs
+++ CheckersTests/MovesTests/KingGetAvailableMovesTests.cs
@@ -80,5 +80,166 @@ namespace CheckersTests.MovesTests
             Assert.Equal(expectedMoves.Count, resultMoves.Count);
             Assert.True(resultMoves.All(r => expectedMoves.Any(e => e.GetType() == r.GetType() && e.From == r.From && e.Target == r.Target)));
         }
+
+        [Fact]
+        public void GetMoves_King_SingleShortKill()
+        {
+            // arrange
+            board.Positions[3, 2].Figure = board.Positions[5, 0].Figure;
+            board.Positions[2, 3].Figure = new WhiteKing();
+
+            var startFigure = board.Positions[2, 3].Figure;
+
+            // path 1
+            var path1killFrom1 = board.Positions[2, 3];
+            var path1killedPosition1 = board.Positions[3, 2];
+            var path1killTarget1 = board.Positions[4, 1];
+
+            var expectedMoves = new List<MoveBase> {
+                new KillMove(path1killFrom1, path1killTarget1, path1killedPosition1)
+            };
+
+            // act
+            var resultMoves = startFigure.GetAvailableMoves(board);
+
+            // assert
+            Assert.Equal(expectedMoves.Count, resultMoves.Count);
+            Assert.True(resultMoves.All(r => expectedMoves.Any(e => e.GetType() == r.GetType() && e.From == r.From && e.Target == r.Target)));
+            var path1kill1 = (resultMoves[0] as KillMove);
+            Assert.Equal(path1kill1.From, path1killFrom1);
+            Assert.Equal(path1kill1.Target, path1killTarget1);
+            Assert.Equal(path1kill1.Killed, path1killedPosition1);
+            Assert.Null(path1kill1.InnerMove);
+        }
+
+        [Fact]
+        public void GetMoves_King_SingleLongKill()
+        {
+            // arrange
+            board.Positions[4, 1].Figure = board.Positions[5, 0].Figure;
+            board.Positions[2, 3].Figure = new WhiteKing();
+
+            var startFigure = board.Positions[2, 3].Figure;
+
+            // path 1
+            var path1killFrom1 = board.Positions[2, 3];
+            var path1killedPosition1 = board.Positions[4, 1];
+            var path1killTarget1 = board.Positions[5, 0];
+
+            var expectedMoves = new List<MoveBase> {
+                new KillMove(path1killFrom1, path1killTarget1, path1killedPosition1)
+            };
+
+            // act
+            var resultMoves = startFigure.GetAvailableMoves(board);
+
+            // assert
+            Assert.Equal(expectedMoves.Count, resultMoves.Count);
+            Assert.True(resultMoves.All(r => expectedMoves.Any(e => e.GetType() == r.GetType() && e.From == r.From && e.Target == r.Target)));
+            var path1kill1 = (resultMoves[0] as KillMove);
+            Assert.Equal(path1kill1.From, path1killFrom1);
+            Assert.Equal(path1kill1.Target, path1killTarget1);
+            Assert.Equal(path1kill1.Killed, path1killedPosition1);
+            Assert.Null(path1kill1.InnerMove);
+        }
+
+        [Fact]
+        public void GetMoves_King_SingleKillStrike()
+        {
+            // arrange
+            board.Positions[4, 1].Figure = board.Positions[5, 0].Figure;
+            board.Positions[7, 2].Figure = null;
+            board.Positions[2, 3].Figure = new WhiteKing();
+
+            var startFigure = board.Positions[2, 3].Figure;
+
+            // path 1
+            var path1killFrom1 = board.Positions[2, 3];
+            var path1killedPosition1 = board.Positions[4, 1];
+            var path1killTarget1 = board.Positions[5, 0];
+
+            var path1killFrom2 = board.Positions[5, 0];
+            var path1killedPosition2 = board.Positions[6, 1];
+            var path1killTarget2 = board.Positions[7, 2];
+
+            var expectedMoves = new List<MoveBase> {
+                new KillMove(path1killFrom1, path1killTarget1, path1killedPosition1)
+                {
+                    InnerMove = new KillMove(path1killFrom2, path1killTarget2, path1killedPosition2)
+                }
+            };
+
+            // act
+            var resultMoves = startFigure.GetAvailableMoves(board);
+
+            // assert
+            Assert.Equal(expectedMoves.Count, resultMoves.Count);
+            Assert.True(resultMoves.All(r => expectedMoves.Any(e => e.GetType() == r.GetType() && e.From == r.From && e.Target == r.Target)));
+            var path1kill1 = (resultMoves[0] as KillMove);
+            Assert.Equal(path1kill1.From, path1killFrom1);
+            Assert.Equal(path1kill1.Target, path1killTarget1);
+            Assert.Equal(path1kill1.Killed, path1killedPosition1);
+            var path1kill2 = path1kill1.InnerMove;
+            Assert.Equal(path1kill2.From, path1killFrom2);
+            Assert.Equal(path1kill2.Target, path1killTarget2);
+            Assert.Equal(path1kill2.Killed, path1killedPosition2);
+            Assert.Null(path1kill2.InnerMove);
+        }
+
+        [Fact]
+        public void GetMoves_King_MultipleKillStrike()
+        {
+            // arrange
+            board.Positions[4, 1].Figure = board.Positions[5, 0].Figure;
+            board.Positions[7, 2].Figure = null;
+            board.Positions[4, 5].Figure = board.Positions[5, 6].Figure;
+            board.Positions[2, 3].Figure = new WhiteKing();
+
+            var startFigure = board.Positions[2, 3].Figure;
+
+            // path 1
+            var path1killFrom1 = board.Positions[2, 3];
+            var path1killedPosition1 = board.Positions[4, 1];
+            var path1killTarget1 = board.Positions[5, 0];
+
+            var path1killFrom2 = board.Positions[5, 0];
+            var path1killedPosition2 = board.Positions[6, 1];
+            var path1killTarget2 = board.Positions[7, 2];
+
+            // path 2
+            var path2killFrom1 = board.Positions[2, 3];
+            var path2killedPosition1 = board.Positions[4, 5];
+            var path2killTarget1 = board.Positions[5, 6];
+
+            var expectedMoves = new List<MoveBase> {
+                new KillMove(path1killFrom1, path1killTarget1, path1killedPosition1)
+                {
+                    InnerMove = new KillMove(path1killFrom2, path1killTarget2, path1killedPosition2)
+                },
+                new KillMove(path2killFrom1, path2killTarget1, path2killedPosition1)
+            };
+
+            // act
+            var resultMoves = startFigure.GetAvailableMoves(board);
+
+            // assert
+            Assert.Equal(expectedMoves.Count, resultMoves.Count);
+            Assert.True(resultMoves.All(r => expectedMoves.Any(e => e.GetType() == r.GetType() && e.From == r.From && e.Target == r.Target)));
+            var path1kill1 = (resultMoves[0] as KillMove);
+            Assert.Equal(path1kill1.From, path1killFrom1);
+            Assert.Equal(path1kill1.Target, path1killTarget1);
+            Assert.Equal(path1kill1.Killed, path1killedPosition1);
+            var path1kill2 = path1kill1.InnerMove;
+            Assert.Equal(path1kill2.From, path1killFrom2);
+            Assert.Equal(path1kill2.Target, path1killTarget2);
+            Assert.Equal(path1kill2.Killed, path1killedPosition2);
+            Assert.Null(path1kill2.InnerMove);
+
+            var path2kill1 = (resultMoves[1] as KillMove);
+            Assert.Equal(path2kill1.From, path2killFrom1);
+            Assert.Equal(path2kill1.Target, path2killTarget1);
+            Assert.Equal(path2kill1.Killed, path2killedPosition1);
+            Assert.Null(path2kill1.InnerMove);
+        }
     }
 }
