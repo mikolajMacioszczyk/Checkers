de2e8b9848147e6564e4995de9827c99f29c90a3
Mikołaj Macioszczyk <macioszczyk@vialutions.com>
2022-04-29 11:18:41 +0200

    implement basic binding

---
+++ DesktopCheckers/BoardField.cs
@@ -0,0 +1,36 @@
+﻿using Checkers.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace DesktopCheckers
+{
+    public class BoardField : INotifyPropertyChanged
+    {
+        private Position position;
+        public Position Position
+        {
+            get { return position; }
+            set 
+            { 
+                position = value;
+                OnPropertyChanged(nameof(IsWhiteMan));
+                OnPropertyChanged(nameof(IsBlackMan));
+            }
+        }
+
+        public bool IsWhiteMan => Position.Figure is WhiteMan;
+        public bool IsBlackMan => Position.Figure is BlackMan;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- DesktopCheckers/MainVindowViewModel.cs
+++ DesktopCheckers/MainVindowViewModel.cs
@@ -1,12 +1,90 @@
-﻿using System;
+﻿using Checkers.Enums;
+using Checkers.Interfaces;
+using Checkers.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DesktopCheckers
 {
-    public class MainVindowViewModel
+    public class MainVindowViewModel : IUserInterface, INotifyPropertyChanged
     {
+        private const int Size = 8;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public BoardField[,] BoardFields { get; set; }
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { 
+                message = value; 
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public MainVindowViewModel()
+        {
+            BoardFields = new BoardField[Size, Size];
+            for (int row = 0; row < Size; row++)
+            {
+                for (int column = 0; column < Size; column++)
+                {
+                    BoardFields[row, column] = new BoardField();
+                }
+            }
+        }
+
+        public void ShowNextMove(IPlayer player)
+        {
+            var color = player.Color == FigureColor.White ? "White" : "Black";
+            Message = $"Player {player.Name} ({color}) move: ";
+        }
+
+        public void ShowMessage(string message)
+        {
+            Message = message;
+        }
+
+        public void GameOver(IPlayer whitePlayer, IPlayer blackPlayer, GameResult result, int moveCount)
+        {
+            switch (result)
+            {
+                case GameResult.WhiteWin:
+                    Message = $"Game over!!! Player {whitePlayer.Name} (White) won in {moveCount} moves";
+                    break;
+                case GameResult.BlackWin:
+                    Message = $"Game over!!! Player {blackPlayer.Name} (Black) won in {moveCount} moves";
+                    break;
+                default:
+                    Message = $"Game over!!! Draw after {moveCount} moves";
+                    break;
+            }
+        }
+
+        public void ShowBoard(Board board)
+        {
+            for (int row = 0; row < board.Size; row++)
+            {
+                for (int column = 0; column < board.Size; column++)
+                {
+                    if (board.IsPositionEnabled(row, column))
+                    {
+                        BoardFields[row, column].Position = board.Positions[row, column];
+                    }
+                }
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
